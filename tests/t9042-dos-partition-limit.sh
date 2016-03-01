ce7a3fd5e2beeee6b0765decb77ba8ee15867342
Brian C. Lane <bcl@redhat.com>
2016-03-01 13:51:54 -0800

    tests: Add wait to t9042 (#1257415)
    Also make the new disklabel and first partition all in one step to
    prevent udev problems.
    
    Resolves: rhbz#1257415

--- tests/t9042-dos-partition-limit.sh
+++ tests/t9042-dos-partition-limit.sh
@@ -44,8 +44,8 @@ printf '%s\n' "BYT;" \
     "1:$((start-2))s:$((n-1))s:$((n-start+2))s:::lba;" \
   > exp || fail=1
 
-parted -s $scsi_dev mklabel msdos || fail=1
-parted -s -a min $scsi_dev mkpart extended $((start-2))s 100% || fail=1
+parted -s -a min $scsi_dev mklabel msdos mkpart extended $((start-2))s 100% || fail=1
+wait_for_dev_to_appear_ ${scsi_dev}1 || fail=1
 
 i=1
 while :; do
