5a9a49776ee98ca86cfe123b79bbee2279f93961
Brian C. Lane <bcl@redhat.com>
2016-12-22 16:59:27 -0800

    tests: Update t0220 and t0280 for the swap flag.

--- tests/t0220-gpt-msftres.sh
+++ tests/t0220-gpt-msftres.sh
@@ -56,7 +56,11 @@ printf "BYT;\n$dev:${n_sectors}s:file:$ss:$ss:gpt::;\n" > exp
 i=1
 for type in $fs_types; do
   end=$(expr $start + $part_size - 1)
-  case $type in fat*|NTFS) flag=msftdata;; *) flag=;; esac
+  case $type in
+     fat*|NTFS) flag=msftdata;;
+     linux-swap) flag=swap;;
+     *) flag=;;
+  esac
   echo "$i:${start}s:${end}s:${part_size}s::$type:$flag;" >> exp || fail=1
   parted -s $dev mkpart p-name $type ${start}s ${end}s > err 2>&1 || fail=1
   compare /dev/null err || fail=1
