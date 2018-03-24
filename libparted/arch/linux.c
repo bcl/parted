6ac7054494df5b6d2e711e30df6f7df280db8776
Richard W.M. Jones <rjones@redhat.com>
2018-03-24 17:37:02 +0000

    linux: Include <sys/sysmacros.h> for major() macro.
    Since glibc 2.27 this header is required.
    
    (cherry picked from commit ba5e0451b51c983e40afd123b6e0d3eddb55e610)

--- libparted/arch/linux.c
+++ libparted/arch/linux.c
@@ -41,6 +41,7 @@
 #include <sys/utsname.h>        /* for uname() */
 #include <scsi/scsi.h>
 #include <assert.h>
+#include <sys/sysmacros.h>
 #ifdef ENABLE_DEVICE_MAPPER
 #include <libdevmapper.h>
 #endif
